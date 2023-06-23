dcc0f80326794e7e8f3eb7dabc6c592fe378ef92
LucasSilvaO <lucas.oliveiraaa20@gmail.com>
2023-06-23 14:50:37 -0300

    Salva

--- artCustomization/urls.py
+++ artCustomization/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from artCustomization.views import art_customization, envio_de_arte
+from artCustomization.views import art_customization, envio_de_arte, arte_enviada
 
 urlpatterns = [
     path('art-customization/<int:idOrder>', art_customization, name='art_customization'),
-    path('envio-de-arte/<int:idOrder>', envio_de_arte, name="envio_de_arte")
+    path('envio-de-arte/<int:idOrder>', envio_de_arte, name="envio_de_arte"),
+    path("arte-enviada", arte_enviada, name="arte_enviada"),
 ]
--- artCustomization/views.py
+++ artCustomization/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from apps.orders.models import Order, OrderByClient
+from django.contrib import messages
 
 def art_customization(request, idOrder):
     print(idOrder)
@@ -11,3 +12,7 @@ def envio_de_arte(request, idOrder):
     print(idOrder)
     pedido = get_object_or_404(Order, idOrder=idOrder)
     return render(request, 'artCustomization/envio_de_arte.html', {"pedido": pedido} )
+
+def arte_enviada(request):
+    messages.success(request, 'Muito Obrigado! Te avisaremos se precisarmos de mais alguma coisa')
+    return render(request, 'artCustomization/teste_aviso.html')
